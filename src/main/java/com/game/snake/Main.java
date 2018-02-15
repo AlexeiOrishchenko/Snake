09d681edb06a24fbdd2e4969991c7ec8d2bc5a94
Niktia Koliadin <qThegamEp@gmail.com>
2018-02-15 16:19:12 +0200

    version 1.10 Edit Main class

--- src/main/java/com/game/snake/Main.java
+++ src/main/java/com/game/snake/Main.java
@@ -4,30 +4,10 @@ import com.game.snake.gui.MainMenuGUI;
 
 /**
  * @author Koliadin Nikita
- * @version 1.9
- *
- * This class has examples how we can start our game
+ * @version 1.10
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-
-        /*
-        Thread thread = new Thread(new MainGUI());
-        thread.start();
-        */
-
-//        /*
-//        ExecutorService executorMainGUI = Executors.newSingleThreadExecutor();
-//        executorMainGUI.execute(new MainGUI());
-//        executorMainGUI.execute(new MainGUI());
-//        executorMainGUI.shutdown();
-//        */
-
-        /*
-        Executors.newSingleThreadExecutor().execute(new MainGUI());
-        */
-
-//        Setting.setExitGUIJLabelList(new ArrayList<JLabel>(Arrays.asList(new JLabel("1"), new JLabel("2"))));
+    public static void main(String[] args) {
         new MainMenuGUI().run();
     }
 }
