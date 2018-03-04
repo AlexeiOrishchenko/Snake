09a75f060a8d8379d1eebcd3984a86816562166b
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-04 12:03:54 +0200

    version 1.13: Refracted Mouse class: - Add lombok;

--- src/main/java/com/game/snake/model/objects/mouse/Mouse.java
+++ src/main/java/com/game/snake/model/objects/mouse/Mouse.java
@@ -1,28 +1,18 @@
 package com.game.snake.model.objects.mouse;
 
-import org.jetbrains.annotations.Contract;
+import lombok.Getter;
 
 /**
  * @author Koliadin Nikita
- * @version 1.10
+ * @version 1.13
  */
 public final class Mouse {
 
-    private final int x;
-    private final int y;
+    @Getter private final int x;
+    @Getter private final int y;
 
     public Mouse(final int x, final int y) {
         this.x = x;
         this.y = y;
-    }
-
-    @Contract(pure = true)
-    public int getX() {
-        return x;
-    }
-
-    @Contract(pure = true)
-    public int getY() {
-        return y;
     }
 }
