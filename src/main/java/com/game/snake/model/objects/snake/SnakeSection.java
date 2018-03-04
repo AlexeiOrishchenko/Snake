aec22f276f331390ed7bf262f359361bbb87af26
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-04 12:18:37 +0200

    version 1.13: Refracted SnakeSection class: - Implemented lombok;

--- src/main/java/com/game/snake/model/objects/snake/SnakeSection.java
+++ src/main/java/com/game/snake/model/objects/snake/SnakeSection.java
@@ -1,49 +1,13 @@
 package com.game.snake.model.objects.snake;
 
-import org.jetbrains.annotations.Contract;
-
-import java.util.Objects;
+import lombok.Data;
 
 /**
  * @author Koliadin Nikita
- * @version 1.10
-
+ * @version 1.13
  */
-public final class SnakeSection {
+@Data public final class SnakeSection {
 
     private final int x;
     private final int y;
-
-    public SnakeSection(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Contract(pure = true)
-    public int getX() {
-        return x;
-    }
-
-    @Contract(pure = true)
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SnakeSection)) {
-            return false;
-        }
-        SnakeSection that = (SnakeSection) o;
-        return getX() == that.getX() &&
-                getY() == that.getY();
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getX(), getY());
-    }
 }
