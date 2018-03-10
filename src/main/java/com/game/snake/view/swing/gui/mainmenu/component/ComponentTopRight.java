af1721c91d7d9cbb965505b5cce1e867a9c155c9
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-10 19:35:37 +0200

    version 1.14 Refracted LabelTopRight class: - New name of class; - Changed name of variables;

---
+++ src/main/java/com/game/snake/view/swing/gui/mainmenu/component/ComponentTopRight.java
@@ -0,0 +1,54 @@
+package com.game.snake.view.swing.gui.mainmenu.component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Koliadin Nikita
+ * @version 1.14
+ */
+public class ComponentTopRight implements MainMenuComponent {
+
+    private final Container container;
+    private final GridBagConstraints gridBagConstraints;
+
+    private JLabel jLabelTopRight;
+
+    @Getter @Setter private String resourceName = String.valueOf("/MainMenuTopRight.gif");
+
+    public ComponentTopRight(@NonNull final Container container,
+                             @NonNull final GridBagConstraints gridBagConstraints) {
+        this.container = container;
+        this.gridBagConstraints = gridBagConstraints;
+    }
+
+    @Override
+    public void init() {
+        loadResource();
+        setGridBagConstraints();
+        container.add(jLabelTopRight, gridBagConstraints);
+    }
+
+    @Override
+    public void setAction() {
+        /* Do nothing */
+    }
+
+    private void loadResource() {
+        if (jLabelTopRight == null) {
+            jLabelTopRight = new JLabel(new ImageIcon(getClass().getResource(resourceName)));
+        }
+    }
+
+    private void setGridBagConstraints() {
+        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.insets = new Insets(0, 0, 0, 0);
+    }
+}
