10a22e2c7c239835a8a0ab7d403a87403b841bb2
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-17 12:23:05 +0200

    version 1.15 RadioButtonColorSnake.class: - Changed RadioButtonColorSnake.class to ComponentColorSnake.class;

--- src/main/java/com/game/snake/view/swing/gui/setting/component/ComponentColorSnake.java
+++ src/main/java/com/game/snake/view/swing/gui/setting/component/ComponentColorSnake.java
@@ -11,16 +11,16 @@ import java.util.List;
 
 /**
  * @author Koliadin Nikita
- * @version 1.14
+ * @version 1.15
  */
-public class RadioButtonColorSnake extends AbstractComponent {
+public class ComponentColorSnake extends AbstractComponent {
 
     private final List<JRadioButton> jRadioButtonColorSnakeList = createColorList();
 
     @Getter @Setter private String text = String.valueOf("Color of the snake: ");
 
-    public RadioButtonColorSnake(@NonNull final Container container,
-                                 @NonNull final GridBagConstraints gridBagConstraints) {
+    public ComponentColorSnake(@NonNull final Container container,
+                               @NonNull final GridBagConstraints gridBagConstraints) {
         super(container, gridBagConstraints);
     }
 
