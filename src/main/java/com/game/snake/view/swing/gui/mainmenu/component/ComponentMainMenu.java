14e9f50f2d1513c229a873a843d2f7f127ed58ba
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-17 15:52:42 +0200

    version 1.15 ComponentMainMenu.class: - Added a more detailed description to the interface method;

--- src/main/java/com/game/snake/view/swing/gui/mainmenu/component/ComponentMainMenu.java
+++ src/main/java/com/game/snake/view/swing/gui/mainmenu/component/ComponentMainMenu.java
@@ -11,12 +11,18 @@ public interface ComponentMainMenu {
      * It should load resources, set the size of the component, set the opaque,
      * give it coordinates on the panel and add it to the container.
      * Implementing this method, you need to consider all these factors.
+     *
+     * This method must be called when you want to initialize a component,
+     * or form components.
      */
     void init();
 
     /**
      * This method must implement the action of the component.
      * It can be implemented by any listener or adapter.
+     *
+     * This method should be called when the component or component initialization
+     * was successful and you want to assign to each component an action
      */
     void setAction();
 }
