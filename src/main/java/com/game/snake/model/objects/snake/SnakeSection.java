fe62867b0489babeeb0c6042b5a6aec0b0d514f9
Niktia Koliadin <qThegamEp@gmail.com>
2018-03-31 12:48:45 +0300

    version 1.17 - SnakeSection.class:   - Added annotations value from Lombok;

--- src/main/java/com/game/snake/model/objects/snake/SnakeSection.java
+++ src/main/java/com/game/snake/model/objects/snake/SnakeSection.java
@@ -1,17 +1,13 @@
 package com.game.snake.model.objects.snake;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
+import lombok.Value;
 
 /**
  * @author Koliadin Nikita
  * @version 1.17
  */
-@Data @AllArgsConstructor @NoArgsConstructor
-public class SnakeSection implements Serializable {
+
+@Value public class SnakeSection {
 
     private int x;
     private int y;
