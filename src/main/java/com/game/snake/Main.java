6e44955c5c8963dd8c149c61f9a41c2eb3b4936b
Niktia Koliadin <qThegamEp@gmail.com>
2018-04-01 11:40:26 +0300

    version 1.17 - Main.class:   - Updated Main;   - Added model and controller;

--- src/main/java/com/game/snake/Main.java
+++ src/main/java/com/game/snake/Main.java
@@ -1,13 +1,21 @@
 package com.game.snake;
 
+import com.game.snake.controller.Controller;
+import com.game.snake.model.Model;
+import com.game.snake.model.ModelInterface;
 import com.game.snake.view.swing.gui.mainmenu.MainMenuGUI;
 
 /**
  * @author Koliadin Nikita
- * @version 1.14
+ * @version 1.17
  */
 public class Main {
     public static void main(String[] args) {
+        ModelInterface model = Model.getInstance();
+        Controller controller = Controller.getInstance();
+
+        controller.setModel(model);
+
         MainMenuGUI.getInstance().run();
     }
 }
